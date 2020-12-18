213ea409e5b83953e5967396222088836e7fe6ed
Philémon Varret <philemon.varret@gmail.com>
2020-12-18 14:03:18 +0100

    Remove compromising info

--- config.php
+++ config.php
@@ -3,12 +3,12 @@ ini_set("display_errors", true);
 date_default_timezone_set("Europe/Paris");  // http://www.php.net/manual/en/timezones.php
 define("DB_DSN", "mysql:host=localhost;dbname=customcms");
 define("DB_USERNAME", "customcms_user");
-define("DB_PASSWORD", "AnyCage39");
+define("DB_PASSWORD", "password");
 define("CLASS_PATH", "classes");
 define("TEMPLATE_PATH", "templates");
 define("HOMEPAGE_NUM_ARTICLES", 5);
 define("ADMIN_USERNAME", "admin");
-define("ADMIN_PASSWORD", "AnyCage39");
+define("ADMIN_PASSWORD", "password");
 require(CLASS_PATH . "/article.php");
 
 function handleException($exception)
