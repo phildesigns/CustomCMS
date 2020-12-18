75ec98bcdd7a7aea506d0853c86d2ac3e11d9ad9
Philémon Varret <philemon.varret@gmail.com>
2020-12-18 04:13:59 +0100

    Fix timezone

--- config.php
+++ config.php
@@ -1,20 +1,21 @@
 <?php
-ini_set( "display_errors", true );
-date_default_timezone_set( "France/Paris" );  // http://www.php.net/manual/en/timezones.php
-define( "DB_DSN", "mysql:host=localhost;dbname=customcms" );
-define( "DB_USERNAME", "customcms_user" );
-define( "DB_PASSWORD", "AnyCage39" );
-define( "CLASS_PATH", "classes" );
-define( "TEMPLATE_PATH", "templates" );
-define( "HOMEPAGE_NUM_ARTICLES", 5 );
-define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", "AnyCage39" );
-require( CLASS_PATH . "/article.php" );
+ini_set("display_errors", true);
+date_default_timezone_set("Europe/Paris");  // http://www.php.net/manual/en/timezones.php
+define("DB_DSN", "mysql:host=localhost;dbname=customcms");
+define("DB_USERNAME", "customcms_user");
+define("DB_PASSWORD", "AnyCage39");
+define("CLASS_PATH", "classes");
+define("TEMPLATE_PATH", "templates");
+define("HOMEPAGE_NUM_ARTICLES", 5);
+define("ADMIN_USERNAME", "admin");
+define("ADMIN_PASSWORD", "AnyCage39");
+require(CLASS_PATH . "/article.php");
 
-function handleException( $exception ) {
+function handleException($exception)
+{
     echo "Sorry, a problem occurred. Please try later.";
-    error_log( $exception->getMessage() );
+    error_log($exception->getMessage());
 }
 
-set_exception_handler( 'handleException' );
+set_exception_handler('handleException');
 ?>
